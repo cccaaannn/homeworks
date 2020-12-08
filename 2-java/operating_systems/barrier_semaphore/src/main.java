2fe44da19e97a0e9e0721ccd83489576e6700b5e
can kurt <can.kurt.aa@gmail.com>
2020-12-09 00:50:08 +0300

    operating_systems added

---
+++ 2-java/operating_systems/barrier_semaphore/src/main.java
@@ -0,0 +1,25 @@
+package furkanin_odevi;
+public class main {
+	
+	public static void main(String[] args){
+
+		barrier_init barrier = new barrier_init(5);
+		
+		Thread a = new Thread(barrier);
+		Thread b = new Thread(barrier);
+		Thread c = new Thread(barrier);
+		Thread d = new Thread(barrier);
+		Thread e = new Thread(barrier);
+		
+
+
+		a.start();
+		b.start();
+		c.start();	
+		d.start();	
+		e.start();	
+		
+	
+	}
+
+}
